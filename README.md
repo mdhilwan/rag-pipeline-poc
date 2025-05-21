94782cb71f185be72f04fb77362e230f16aa1ae2
Md Hilwan <wan.wanted.1@gmail.com>
2025-05-21 14:41:43 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# RAG PoC
+
+Download from this link: [tinyllama](https://huggingface.co/TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF/tree/main?show_file_info=tinyllama-1.1b-chat-v1.0.Q4_0.gguf) and saved it to `./modals/`
+
+Run python: `python main.py`
---
+++ server.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+app.mount("/", StaticFiles(directory="ui", html=True), name="ui")
+
+@app.post("/ask")
+async def ask(request: Request):
+    body = await request.json()
+    query = body.get("query")
+    answer = qa.run(query)
+    return JSONResponse(content={"result": answer})
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("server:app", host="127.0.0.1", port=8000, reload=True)
